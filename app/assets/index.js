fc9a195c221de9adf5fcf15cf2aa3defc00be1e7
ChristYoga123 <christianuswibisono@gmail.com>
2023-09-03 11:52:07 +0700

    [FIX] fix asset dir

--- app/assets/index.js
+++ app/assets/index.js
@@ -1,11 +1,11 @@
 // SplashScreen
-import SplashLogo from "./SplashScreen/logo.webp"
-import SplashCircle from "./SplashScreen/circle.webp"
+import SplashLogo from "./images/SplashScreen/logo.webp"
+import SplashCircle from "./images/SplashScreen/circle.webp"
 
 // Onboarding
-import OnboardingLogo from "./Onboarding/logo.webp"
+import OnboardingLogo from "./images/Onboarding/logo.webp"
 
 // Articles
-import ArticlesHero from "./Articles/hero.webp";
-import ArticlesDummy from "./Articles/dummy.webp";
+import ArticlesHero from "./images/Articles/hero.webp";
+import ArticlesDummy from "./images/Articles/dummy.webp";
 export {SplashLogo, SplashCircle, OnboardingLogo, ArticlesHero, ArticlesDummy}
